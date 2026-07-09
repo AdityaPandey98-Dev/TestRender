c69cae9e2c3d08c0781419403bfee29c4e1a196d
AdityaPandey98-Dev <adityapandey98.dev@gmail.com>
2026-07-09 15:01:43 +0530

    Update

---
+++ src/main/java/com/abcd/testrender/TestRenderApplication.java
@@ -0,0 +1,13 @@
+package com.abcd.testrender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abcd/testrender/TestRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcd.testrender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
